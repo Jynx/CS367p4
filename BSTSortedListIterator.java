f35f729c80ba40ffd7b098e543a0890f8856ec37
Jynx <sevenex@gmail.com>
2014-01-13 10:47:32 -0600

    Update BSTSortedListIterator.java

--- BSTSortedListIterator.java
+++ BSTSortedListIterator.java
@@ -1,17 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   ALL STUDENTS COMPLETE THESE SECTIONS
-// Main Class File:  (WebDictionary.java)
-// Files:            (BSTSortedListIterator.java)
-// Semester:         CS302 Spring 2013
-//
-// Author:           (Steven Volocyk Sevenex@gmail.com)
-// CS Login:         (volocyk)
-// Lecturer's Name:  (Jim Skrentny)
-// Lab Section:      (Lecture 2, 204 Educational Sciences)
-//                   
-// Credits:          
-//////////////////////////// 80 columns wide /////////////////////////////////
-
 import java.util.*;
 
 /**
